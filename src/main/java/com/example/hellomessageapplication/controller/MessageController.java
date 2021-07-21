279176176a0dfa84fca0f9a02d22d4bc0b089ce2
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-07-21 18:50:07 +0530

    RestAPI Call Using Query Parameter

--- src/main/java/com/example/hellomessageapplication/controller/MessageController.java
+++ src/main/java/com/example/hellomessageapplication/controller/MessageController.java
@@ -1,8 +1,7 @@
 package com.example.hellomessageapplication.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/msg")
@@ -11,5 +10,10 @@ public class MessageController {
     @RequestMapping(value = {"/home"})
     public String helloMessage() {
         return "Hello From BridgeLabz !";
+    }
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHelloMark(@RequestParam(value="name")String name) {
+        return "Hello " + name + " !!";
     }
 }
